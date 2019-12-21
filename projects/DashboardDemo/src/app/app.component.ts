502f8acd51c38bcae4aa111fa4c95e76b7ceaac1
Souvik Dutta <duttasouvik654@gmail.com>
2019-12-21 18:24:43 +0530

    tried form element changes and used another project as library

--- projects/DashboardDemo/src/app/app.component.ts
+++ projects/DashboardDemo/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DashboardDemo';
+  profileForm: FormGroup;
+  countryName = 'India';
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.profileForm = this.fb.group({
+      country: ['India', Validators.required],
+      state: ['', Validators.required]
+    })
+  }
+  changeStateField() {
+    this.countryName = this.profileForm.controls.country.value;
+    this.profileForm.controls.state.setValue('');
+  }
+  onSubmit() {
+    console.log(this.profileForm);
+  }
 }
--- projects/DashboardDemo/src/app/app.module.ts
+++ projects/DashboardDemo/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxDashboardModule } from 'ngx-dashboard';
+import { PushComponent} from '../../../../node_modules/push-notification/src/app/push-notification/components/push/push.component';
+import { PushNotificationModule } from '../../../../node_modules/push-notification/src/app/push-notification/push-notification.module';
+import { UserRegHeaderModule } from '../../../../node_modules/push-notification/src/app/user-reg-header/user-reg-header.module';
+import { MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule } from '@angular/material'
 
 @NgModule({
   declarations: [
@@ -12,7 +19,15 @@ import { NgxDashboardModule } from 'ngx-dashboard';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxDashboardModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgxDashboardModule,
+    UserRegHeaderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
