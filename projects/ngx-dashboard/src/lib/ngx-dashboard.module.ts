fbdca2cc11b211fd3f0b1cbb79480ef29983595b
Souvik Dutta <duttasouvik654@gmail.com>
2019-12-22 14:33:50 +0530

    Just added one more component

--- projects/ngx-dashboard/src/lib/ngx-dashboard.module.ts
+++ projects/ngx-dashboard/src/lib/ngx-dashboard.module.ts
@@ -3,14 +3,15 @@ import { NgxDashboardComponent } from './ngx-dashboard.component';
 import { MatCardModule, MatButtonModule } from '@angular/material';
 import { SemiCircularProgressbarComponent } from './semi-circular-progressbar/semi-circular-progressbar.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 
 @NgModule({
-  declarations: [NgxDashboardComponent, SemiCircularProgressbarComponent, TestComponentComponent],
+  declarations: [NgxDashboardComponent, SemiCircularProgressbarComponent, TestComponentComponent, PieChartComponent],
   imports: [
     MatCardModule,
     MatButtonModule
   ],
-  exports: [NgxDashboardComponent, SemiCircularProgressbarComponent]
+  exports: [NgxDashboardComponent, SemiCircularProgressbarComponent, PieChartComponent]
 })
 export class NgxDashboardModule { }
